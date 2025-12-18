61563c10ebf6b81f33b98b872c0ebd00d7466f15
Syed fatema Suchi <137265169+syedfatemasuchi@users.noreply.github.com>
2025-12-18 21:50:42 +0600

    Add files via upload

---
+++ Condition 1.py
@@ -0,0 +1,15 @@
+#Traffic Light 
+light = input("light color : ")
+
+if(light == "red"):
+    print("Stop")
+elif(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("Look")
+else:
+    print ("light is broken")
+    
+
+    
+    
