c49ab70137ba3798d81953666fa4974c17c4b4e8
Syed fatema Suchi <137265169+syedfatemasuchi@users.noreply.github.com>
2025-12-18 23:49:06 +0600

    Add files via upload

---
+++ Condition 2.py
@@ -0,0 +1,12 @@
+#Grade sheet
+
+num = int(input("Number : "))
+
+if(num>=90):
+    print("A")
+elif(num>=80 and num<90):
+    print("B")
+elif(num>=70 and num<80):
+    print("C")
+else:
+    print("D")
