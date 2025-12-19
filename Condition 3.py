b1bfe0672f2e334799f0e2b8a8170739af95bb10
Syed fatema Suchi <137265169+syedfatemasuchi@users.noreply.github.com>
2025-12-19 10:10:51 +0600

    Add files via upload

---
+++ Condition 3.py
@@ -0,0 +1,12 @@
+# sinhgle line If/ Ternary operator 
+
+ #syntax <var> = <var1> if <condition> else <val2>
+
+food = input("food : ")
+eat = "yes" if food == "cake" else "no"
+print(eat)
+
+#<stm1>if<condition>else<stm2)
+
+food = input("food : ")
+print ("sweet") if food == 'cake' or food == 'misti' else print ("not sweet")
