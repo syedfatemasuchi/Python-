aaecbafa1298ccd6a47322c73f118e3495ec787e
Syed fatema Suchi <137265169+syedfatemasuchi@users.noreply.github.com>
2025-12-19 15:02:58 +0600

    Add files via upload

---
+++ Practice 2 (condition).py
@@ -0,0 +1,12 @@
+#Check whether a number is positive, negative, or zero.
+
+Num = int( input ("Number : "))
+
+if(Num > 0):
+    print("Positive")
+elif(Num == 0):
+    print ("Zero")
+else:
+    print("Negetive")
+
+
