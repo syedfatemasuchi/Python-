bf893a152e15ad6165dfcb7983dbc994fff3c253
Syed fatema Suchi <137265169+syedfatemasuchi@users.noreply.github.com>
2025-12-19 16:11:09 +0600

    Add files via upload

---
+++ Practice 3 (Condition)).py
@@ -0,0 +1,10 @@
+
+#Find the largest of three numbers.
+num1, num2, num3 = map(int,input().split())
+
+if(num1>num2 and num1>num3):
+   print(num1)
+elif(num2>num1 and num2>num3):
+   print(num2)
+else:
+   print(num3)
