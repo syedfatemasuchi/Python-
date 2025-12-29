0e5efe6e7ee88ef7459abb0acfc22297eec5e75d
Syed fatema Suchi <137265169+syedfatemasuchi@users.noreply.github.com>
2025-12-29 14:58:25 +0600

    Add files via upload

---
+++ Dictionary 1.py
@@ -0,0 +1,12 @@
+#Create a dictionary with 3 key–value pairs and print the value of a given key.
+
+dict ={
+
+"name": "Suchi",
+"age": "25",
+"Nationality" : "Bangladesh"
+
+}
+print (dict["name"])
+print(dict["age"])
+print(dict["Nationality"])
