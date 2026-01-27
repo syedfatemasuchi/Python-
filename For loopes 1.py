fdaa43f3c08111cd615b62d98e381d53ed1088b4
Syed fatema Suchi <137265169+syedfatemasuchi@users.noreply.github.com>
2026-01-28 00:11:44 +0600

    Add files via upload

---
+++ For loopes 1.py
@@ -0,0 +1,18 @@
+#skip even number
+nums = [1,4,9,16,25,36,49,64,81,100]
+idx=0
+for val in nums:
+    if(val == 9):
+        print("found",idx)
+        break
+    print(val)
+    idx+=1
+
+        
+        
+
+
+
+
+
+
